932bda86036ca9bb887bbceacfae47d6a58bca2b
Yago Miron <yagomiron.exe@gmail.com>
2025-12-23 01:34:12 -0400

    agora vai

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,24 @@ import { initBitrix } from "./hooks/useBitrix";
 import { routes } from "./routes/Routes";
 import "./App.css";
 
+declare global {
+  interface Window {
+    BX24: any;
+  }
+}
+
 function App() {
   useEffect(() => {
     initBitrix().then(() => {
       console.log("Bitrix pronto");
+      window.BX24.callMethod("user.current", {}, (result: any) => {
+        if (result.error()) {
+          console.error("Erro ao buscar usuário:", result.error());
+          return;
+        }
+        const user = result.data();
+        console.log("Usuário Bitrix:", user.NAME, user.LAST_NAME);
+      });
     });
   }, []);
   const elements = useRoutes(routes);
--- src/hooks/useBitrix.ts
+++ src/hooks/useBitrix.ts
@@ -7,18 +7,12 @@ declare global {
 export function initBitrix(): Promise<void> {
   return new Promise((resolve) => {
     if (!window.BX24) {
-      console.log();
-
       console.error("BX24 não está disponível");
       return;
     }
 
     window.BX24.init(() => {
       console.log("BX24 inicializado");
-
-      // Ajusta o tamanho da iframe
-      window.BX24.resizeWindow(1200, 800);
-
       resolve();
     });
   });
